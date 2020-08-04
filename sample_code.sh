339facb12e82489ec48d618422b4d1a3bff9e809
praveen-akepati <45997515+praveen-akepati@users.noreply.github.com>
2020-08-04 17:34:06 +0530

    4th commit

--- sample_code.sh
+++ sample_code.sh
@@ -4,7 +4,7 @@
 echo "This is an Samples Script from GitHub1"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
-echo "This is 3rd webhook"
+echo "This is 4th webhook"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
